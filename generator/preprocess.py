30ad80d895c9af8c7ae39034887f056004dabcd1
Jordan Hamill <jordan@moneykit.com>
2023-04-23 19:47:59 +0100

    Limit products and countries

--- generator/preprocess.py
+++ generator/preprocess.py
@@ -7,6 +7,24 @@ import yaml
 SCRIPT_DIR = pathlib.Path(__file__).parent
 
 
+def expose_only_supported_products(spec: dict) -> None:
+    path = "components/schemas/Products/enum"
+    available: set[str] = set(dpath.get(spec, path))
+    assert available, f"Missing {path}"
+    supported = set(["assets", "auth", "balance", "identity", "transactions"])
+    assert supported.issubset(available)
+    dpath.set(spec, path, list(supported))
+
+
+def expose_only_supported_country_codes(spec: dict) -> None:
+    path = "components/schemas/CountryCode/enum"
+    available: set[str] = set(dpath.get(spec, path))
+    assert available, f"Missing {path}"
+    supported = set(["US", "GB", "CA"])
+    assert supported.issubset(available)
+    dpath.set(spec, path, list(supported))
+
+
 def main() -> None:
     print("reading yaml")
     if len(sys.argv) != 3:
@@ -17,27 +35,25 @@ def main() -> None:
     openapi_output_file = sys.argv[2]
 
     with open(openapi_input_file, "r") as f:
-        y = yaml.safe_load(f)
+        spec = yaml.safe_load(f)
+
+    expose_only_supported_products(spec)
+    expose_only_supported_country_codes(spec)
 
     # delete fields we don't yet support
     # NOTE this just eliminates all fields with these names.
     # E.g., it eliminates ClientProvidedTransaction.location, which we might one day want.
     # If you need to be precise, use more exact expressions.
-
-    for loc in ["**/location",
-                "**/payment_meta",
-                "**/payment_channel"
-                ]:
+    for loc in ["**/location", "**/payment_meta", "**/payment_channel"]:
         try:
-            count = dpath.delete(y, loc)
+            count = dpath.delete(spec, loc)
         except dpath.exceptions.PathNotFound:
             count = 0
         print("deleted", count, loc)
 
-
     print("writing yaml")
     with open(openapi_output_file, "w") as f:
-        yaml.dump(y, f, sort_keys=False)
+        yaml.dump(spec, f, sort_keys=False)
 
 
 if __name__ == "__main__":
