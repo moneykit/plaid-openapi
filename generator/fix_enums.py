78d314afdf85abcebf201f27e021b84966de5c44
Jordan Hamill <jordan@moneykit.com>
2023-04-23 13:48:20 +0100

    Handle nullable enums

--- generator/fix_enums.py
+++ generator/fix_enums.py
@@ -1,5 +1,8 @@
-from pathlib import Path
 import json
+import re
+import textwrap
+from pathlib import Path
+
 
 def fix_enums():
     fixed = 0
@@ -9,12 +12,38 @@ def fix_enums():
         if len(code) > 1:
             name = code[1].strip()
             # "if v" required because Plaid sometimes null in their enum list, for some reason
-            vals = [v for v in json.loads(code[2])['enum'] if v]
-            p.write_text("\n".join((
-                code[0].strip(),
-                "\nfrom strenum import StrEnum\n",
-                f'{name} = StrEnum("{name}", names={vals})'
-            )))
+            model_data = json.loads(code[2])
+            model_type = model_data['type']
+            nullable = model_data.get("nullable", False)
+            vals: list[str] = [v for v in model_data['enum'] if v]
+
+            def clean(varStr):
+                return re.sub('\W|^(?=\d)','_', varStr)
+            names = [clean(v.upper()) for v in vals]
+
+            assert model_type == "string", "Not setup to handle other enum types"
+
+            fixed_enum = code[0].strip() + textwrap.dedent(f"""
+
+            from enum import Enum
+
+
+            class {name}(str, Enum):
+            """)
+            for name, value in zip(names, vals):
+                assert name.isidentifier(), f"{name} is not a valid identifier"
+                fixed_enum += f'    {name} = "{value}"\n'
+
+            if nullable:
+                fixed_enum += textwrap.dedent("""
+                # Nullable OpenAPI enum
+                    @classmethod
+                    def __modify_schema__(cls, field_schema: dict) -> None:
+                        field_schema["nullable"] = True
+                """)
+
+            p.write_text(fixed_enum)
+
             fixed = fixed + 1
     print(f"{fixed=}")
 
