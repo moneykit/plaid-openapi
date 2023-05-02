29473ebce386aae6f24b4f7eb3117f5aa18746f8
Jordan Hamill <jordan@moneykit.com>
2023-05-02 14:17:49 +0100

    Update preprocessor

--- generator/preprocess.py
+++ generator/preprocess.py
@@ -25,6 +25,79 @@ def expose_only_supported_country_codes(spec: dict) -> None:
     dpath.set(spec, path, list(supported))
 
 
+def remove_unsupported_properties(spec: dict) -> None:
+    remove_properties = {
+        "LinkTokenCreateRequest": [
+            "link_customization_name",
+            "android_package_name",
+            "institution_data",
+            "account_filters",
+            "eu_config",
+            "institution_id",
+            "payment_initiation",
+            "deposit_switch",
+            "employment",
+            "income_verification",
+            "auth",
+            "transfer",
+            "update",
+            "identity_verification",
+            "user_token",
+            "investments",
+        ],
+        "LinkTokenCreateRequestUser": [
+            "legal_name",
+            "name",
+            "ssn",
+            "date_of_birth",
+            "address",
+            "id_number",
+        ],
+        "Item": [
+            "consent_expiration_time",
+            "update_type",
+        ],
+        "AccountBase": [
+            "official_name",
+            "verification_status",
+            "persistent_account_id",
+        ],
+        "AccountBalance": [
+            "unofficial_currency_code",
+            "last_updated_datetime",
+        ],
+        "NumbersACH": [
+            "can_transfer_in",
+            "can_transfer_out",
+        ],
+        "Email": [
+            "type",
+        ],
+        "TransactionsGetRequestOptions": [
+            "include_original_description",
+            "include_personal_finance_category_beta",
+            "include_logo_and_counterparty_beta",
+        ],
+        "Transaction": [
+            "pending_transaction_id",
+            "unofficial_currency_code",
+            "location",
+            "payment_meta",
+            "payment_channel",
+            "account_owner",
+            "authorized_date",
+            "authorized_datetime",
+        ],
+    }
+
+    for schema_name, properties_to_remove in remove_properties.items():
+        path = f"components/schemas/{schema_name}/properties/"
+        count = 0
+        for prop in properties_to_remove:
+            count += dpath.delete(spec, path + prop)
+        print(f"Deleted {count} properties from {path}")
+
+
 def main() -> None:
     print("reading yaml")
     if len(sys.argv) != 3:
@@ -39,17 +112,7 @@ def main() -> None:
 
     expose_only_supported_products(spec)
     expose_only_supported_country_codes(spec)
-
-    # delete fields we don't yet support
-    # NOTE this just eliminates all fields with these names.
-    # E.g., it eliminates ClientProvidedTransaction.location, which we might one day want.
-    # If you need to be precise, use more exact expressions.
-    for loc in ["**/location", "**/payment_meta", "**/payment_channel"]:
-        try:
-            count = dpath.delete(spec, loc)
-        except dpath.exceptions.PathNotFound:
-            count = 0
-        print("deleted", count, loc)
+    remove_unsupported_properties(spec)
 
     print("writing yaml")
     with open(openapi_output_file, "w") as f:
