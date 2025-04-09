6a7baed3b4b5ca15ef2168c0c4445d034a573f79
Andrea Castellanos <andrea@moneykit.com>
2025-04-09 10:18:53 -0400

    rename imports

--- skeleton/src/plaid_skel/models/item_status_transactions.py
+++ skeleton/src/plaid_skel/models/item_status_transactions.py
@@ -5,7 +5,8 @@
 
 
 from __future__ import annotations
-from datetime import date, datetime  # noqa: F401
+from datetime import date as date_  # noqa: F401
+from datetime import datetime as datetime_  # noqa: F401
 
 import re  # noqa: F401
 from typing import Any, Dict, List, Optional  # noqa: F401
